7516bab1ce8d07ffa26e55ce83055f0bd7a5cae8
Leonid <37238606+bs4ivc@users.noreply.github.com>
2022-01-23 17:28:39 +0200

    add static address www.google.com

--- ping_script.ps1
+++ ping_script.ps1
@@ -1,5 +1,4 @@
-$Computer = Read-Host -Prompt "Enter IP address"
-$Ping = Test-Connection -Count 10 -ComputerName $Computer
+$Ping = Test-Connection -Count 10 -ComputerName www.google.com
 ForEach ($Result in $Ping) {
   If ($Result.ResponseTime -lt 100) {
     $Result | Select-Object -Property Address,BufferSize,ResponseTime | Write-Host -BackgroundColor Green
