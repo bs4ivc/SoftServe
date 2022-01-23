8a4c4d948642748575cf6ff3f08f323b96d77313
Leonid <37238606+bs4ivc@users.noreply.github.com>
2022-01-23 16:36:41 +0200

    test

--- script.sh
+++ script.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-ping -c1 www.google.com > /dev/null
-if [ $? -eq 0 ]
-  then 
-    echo "www.google.com is available!" 
-    exit 0
-  else
-    echo "www.google.com is not available"
-fi
-
+ping www.google.com
