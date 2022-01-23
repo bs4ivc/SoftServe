0fd3266fae400dc9fd00f9996c5c2fd9ca61c132
Leonid <37238606+bs4ivc@users.noreply.github.com>
2022-01-23 17:17:03 +0200

    Update ping_script.java

--- ping_script.java
+++ ping_script.java
@@ -1,7 +1,7 @@
 public class PingExample {
     public static void main(String[] args){
         try{
-            InetAddress address = InetAddress.getByName("192.168.1.103");
+            InetAddress address = InetAddress.getByName("8.8.8.8");
             boolean reachable = address.isReachable(10000);
 
             System.out.println("Is host reachable? " + reachable);
